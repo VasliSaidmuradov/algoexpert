8b7bdbebb9a6775f03e6ebf15ae0076ed0a39fc2
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-10-10 21:13:31 +0600

    feat: Max Path Sum in BST - hard Python one more solution

--- binary trees/hard_maxPathSum.py
+++ binary trees/hard_maxPathSum.py
@@ -24,3 +24,38 @@ def findMaxSum(tree):
   maxPathSum = max(leftMaxPathSum, rightMaxPathSum, maxSumAsRootNode)
 
   return (maxSumAsBranch, maxPathSum)
+
+
+# 2
+def maxPathSum(tree):
+  return getMaxPathSum(tree).maxSum
+
+
+class TreeInfo:
+  def __init__(self, currSum, maxSum):
+    self.currSum = currSum
+    self.maxSum = maxSum
+
+def getMaxPathSum(tree):
+  if tree is None:
+    return TreeInfo(0, float('-inf'))
+
+  leftSubTreeInfo = getMaxPathSum(tree.left)
+  rightSubTreeInfo = getMaxPathSum(tree.right)
+
+  # without root node
+  maxSumNoRoot = max(leftSubTreeInfo.maxSum, rightSubTreeInfo.maxSum)
+
+  # with root node
+  # case1: contain single side
+  maxSumSingleSide = max(leftSubTreeInfo.currSum, rightSubTreeInfo.currSum) + tree.value
+  # case2: contain both side
+  maxSumBothSide = leftSubTreeInfo.currSum + rightSubTreeInfo.currSum + tree.value
+  # case3: contain just itself
+  maxSumSingleRoot = tree.value
+
+  maxSum = max(maxSumNoRoot, maxSumSingleSide, maxSumBothSide, maxSumSingleRoot)
+  currSum = max(maxSumSingleSide, tree.value)
+
+  return TreeInfo(currSum, maxSum)
+    
