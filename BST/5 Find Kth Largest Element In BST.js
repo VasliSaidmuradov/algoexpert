41a33f2bb0ac18729eceb22758416421e7bdf5e1
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-10-13 21:11:25 +0600

    feat: Find Kth Largest Element In BST - Medium - JS

---
+++ BST/5 Find Kth Largest Element In BST.js
@@ -0,0 +1,47 @@
+// Find Kth Largest Element In BST
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// #1
+function findKthLargestValueInBst(tree, k) {
+  let latestVisited = -1
+  let numberOfVisited = 0
+
+  const inOrder = (tree) => {
+    if (tree.right) inOrder(tree.right)
+
+    if (numberOfVisited < k) {
+      latestVisited = tree.value
+      numberOfVisited++
+    }
+
+    if (tree.left) inOrder(tree.left)
+  }
+
+  inOrder(tree)
+
+  return latestVisited
+}
+
+// #2
+function findKthLargestValueInBst(tree, k) {
+  const arr = []
+  
+  const inOrder = (tree) => {
+    if (arr.length < k) {
+      if (tree.right) inOrder(tree.right)
+      arr.push(tree.value)
+      if (tree.left) inOrder(tree.left)
+    }
+  }
+
+  inOrder(tree)
+
+  return arr[k-1]
+}
