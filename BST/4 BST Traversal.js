7233f8eaff4df458bca046144a76999de584b3f4
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-10-13 20:33:23 +0600

    feat: BST Traversal - Medium - JS

--- BST/4 BST Traversal.js
+++ BST/4 BST Traversal.js
@@ -1,3 +1,66 @@
 // BST Traversal - Medium
 
 // #1
+function inOrderTraverse(tree, array) {
+  if (tree.left) inOrderTraverse(tree.left, array)
+  array.push(tree.value)
+  if (tree.right) inOrderTraverse(tree.right, array)
+  return array
+}
+
+function preOrderTraverse(tree, array) {
+  array.push(tree.value)
+  if (tree.left) preOrderTraverse(tree.left, array)
+  if (tree.right) preOrderTraverse(tree.right, array)
+  return array
+}
+
+function postOrderTraverse(tree, array) {
+  if (tree.left) postOrderTraverse(tree.left, array)
+  if (tree.right) postOrderTraverse(tree.right, array)
+  array.push(tree.value)
+  return array
+}
+
+// #2
+function inOrderTraverse(tree, array) {
+  const helper = (tree, array) => {
+    if (!tree) return
+
+    helper(tree.left, array)
+    array.push(tree.value)
+    helper(tree.right, array)
+  }
+
+  helper(tree, array)
+
+  return array
+}
+
+function preOrderTraverse(tree, array) {
+  const helper = (tree, array) => {
+    if (!tree) return
+
+    array.push(tree.value)
+    helper(tree.left, array)
+    helper(tree.right, array)
+  }
+
+  helper(tree, array)
+
+  return array
+}
+
+function postOrderTraverse(tree, array) {
+  const helper = (tree, array) => {
+  if (!tree) return
+
+  helper(tree.left, array)
+  helper(tree.right, array)
+  array.push(tree.value)
+  }
+
+  helper(tree, array)
+
+  return array
+}
