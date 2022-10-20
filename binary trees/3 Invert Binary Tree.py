61bf937d32268e43b8a6840640c1d25d1446c845
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-10-21 01:47:00 +0600

    feat: Invert Binary Tree - Python

--- binary trees/3 Invert Binary Tree.py
+++ binary trees/3 Invert Binary Tree.py
@@ -1,2 +1,26 @@
 # Invert Binary Tree
 
+
+# This is the class of the input binary tree.
+class BinaryTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+# 1
+def invertBinaryTree(tree):
+  if tree:
+    tree.left, tree.right = tree.right, tree.left
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+# 2
+def invertBinaryTree(tree):
+  queue = [ tree ]
+
+  while queue:
+    curr = queue.pop(0)
+    curr.left, curr.right = curr.right, curr.left
+    if curr.left: queue.append(curr.left)
+    if curr.right: queue.append(curr.right)
