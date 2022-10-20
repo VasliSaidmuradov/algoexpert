3c5fe60ed4d0148bc95f634a696e3d0f4d145e0a
Vasli Saidmuradov <vasli.said@gmail.com>
2022-10-20 15:47:11 +0600

    feat: Next Greater Element - Python

--- stacks/5 Next Greater Element.py
+++ stacks/5 Next Greater Element.py
@@ -1,1 +1,35 @@
 # Next Greater Element
+
+# 1
+def nextGreaterElement(array):
+  res = [-1] * len(array)
+  stack = []
+
+  for idx in range(len(array) * 2):
+    currIdx = idx % len(array)
+
+    while stack and array[currIdx] > array[stack[len(stack) - 1]]:
+      topOfStack = stack.pop()
+      res[topOfStack] = array[currIdx]
+    
+    stack.append(currIdx)
+
+  return res
+
+  # 2
+def nextGreaterElement(array):
+  stack = []
+  res = [-1] * len(array)
+
+  for idx in reversed(range(2* len(array))):
+    currIdx = idx % len(array)
+
+    while stack and stack[len(stack) - 1] <= array[currIdx]:
+      stack.pop()
+
+    if stack:
+      res[currIdx] = stack[len(stack) - 1]
+
+    stack.append(array[currIdx])
+
+  return res
